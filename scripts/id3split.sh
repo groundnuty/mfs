247397dd9efee2589828f2382f6f8b65bc5fb56f
groundnuty <grounnduty@gmail.com>
2014-07-24 07:10:46 +0200

    updated example data catalog for next version

--- scripts/id3split.sh
+++ scripts/id3split.sh
@@ -6,12 +6,12 @@
 #   Third, if supplied, a destination file for the rest of the file
 #
 
-id3_header_size=$(python ./id3size.py $1) ;
+id3_header_size=$(python ./id3size.py "$1") ;
 
 if [ -n "$2" ]; then
-    head -c+$id3_header_size $1 > $2
+    head -c+$id3_header_size "$1" > "$2"
 fi
 
 if [ -n "$3" ]; then
-    tail -c+$((id3_header_size+1)) $1 > $3
+    tail -c+$((id3_header_size+1)) "$1" > "$3"
 fi
